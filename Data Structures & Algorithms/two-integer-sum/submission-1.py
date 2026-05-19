86531d99b1a0ac1b209dbc0c2d274b884d194719
Cholan Kinnera <imcholan.kinnera@gmail.com>
2026-05-19 23:06:21 +0530

    Bulk sync: 13 submissions

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        s = {}
+        for i in range(len(nums)):
+            n = target - nums[i]
+
+            if n in s:
+                return [s[n] , i]
+
+            s[nums[i]] = i
